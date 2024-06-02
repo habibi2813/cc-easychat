fb23a03d3c692193bbfea772898a373c5e1710d3
nohlstd <kuba.pawel44@gmail.com>
2024-06-02 20:15:27 +0200

    updating server.lua

--- server.lua
+++ server.lua
@@ -5,12 +5,12 @@ local main = basalt.createFrame()
 local aButton = main:addButton()
         :setText("Host chat")
         :setPosition(4, 4)
-        :setSize(16, 4)
+        :setSize(16, 5)
 
 local ServerSize = main:addInput()
         :setInputType("number")
         :setPosition(4, 10)
-        :setSize(16, 4)
+        :setSize(16, 5)
 
 
 
