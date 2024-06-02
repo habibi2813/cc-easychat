ecf6983c07864785e4f1686c5a77dab77dae0b2c
nohlstd <kuba.pawel44@gmail.com>
2024-06-02 19:50:55 +0200

    server.lua update

--- server.lua
+++ server.lua
@@ -1,10 +1,19 @@
 local basalt = require("basalt")
 
 local main = basalt.createFrame()
+
 local aButton = main:addButton()
         :setText("Host chat")
-        :setPosition(2, 2)
+        :setPosition(4, 4)
         :setSize(16, 4)
+
+local ServerSize = main:addInput()
+        :setInputType("number")
+        :setPosition(8, 4)
+        :setSize(16, 4)
+
+
+
 
 basalt.autoUpdate()
 
