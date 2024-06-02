dc8c8a492f29a5c369d6f6299a5065102e82e93a
nohlstd <kuba.pawel44@gmail.com>
2024-06-02 20:33:27 +0200

    Added background to server

--- server.lua
+++ server.lua
@@ -2,15 +2,19 @@ local basalt = require("basalt")
 
 local main = basalt.createFrame()
 
+local aPane = mainFrame:addPane()
+        :setSize(30, 10)
+        :setBackground(colors.yellow)
+
 local aButton = main:addButton()
         :setText("Host chat")
         :setPosition(4, 4)
-        :setSize(16, 5)
+        :setSize(16, 4)
 
 local ServerSize = main:addInput()
         :setInputType("number")
         :setPosition(4, 10)
-        :setSize(16, 5)
+        :setSize(16, 4)
 
 
 
