99889feea18148b3562cfa518d70675fb27de16f
habibi2813 <68748628+habibi2813@users.noreply.github.com>
2024-06-03 20:14:41 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -5,10 +5,17 @@ local mainFrame = basalt.createFrame()
 
 local modem = peripheral.find("modem") or error("No modem attached", 0)
 
-local aButton = main:addButton()
+local HostButton = main:addButton()
         :setText("Host chat")
         :setPosition(4, 4)
         :setSize(16, 4)
+
+HostButton:onClick(function(self,event,button,x,y)
+  if(event=="mouse_click")and(button==1)then
+    basalt.debug("Left mousebutton got clicked!")
+  end
+end)
+
 
 basalt.autoUpdate()
 
