33dabcf2e106a4416834f66c6cd55d4e65a27c63
habibi2813 <68748628+habibi2813@users.noreply.github.com>
2024-06-03 20:05:48 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -3,13 +3,16 @@ local basalt = require("basalt")
 local main = basalt.createFrame()
 local mainFrame = basalt.createFrame()
 
+local modem = peripheral.find("modem") or error("No modem attached", 0)
+
 local aButton = main:addButton()
         :setText("Host chat")
         :setPosition(4, 4)
         :setSize(16, 4)
 
-local modem type = modem.isWireless()
-
-local modem = peripheral.wrap("right")
-
 basalt.autoUpdate()
+
+
+
+
+
