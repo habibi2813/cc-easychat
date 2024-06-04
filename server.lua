64f10ba0910326dd47629cdb2c1f00b9fa5b543c
nohlstd <kuba.pawel44@gmail.com>
2024-06-04 10:57:00 +0200

    updating server.lua

--- server.lua
+++ server.lua
@@ -3,16 +3,24 @@ local basalt = require("basalt")
 local main = basalt.createFrame()
 local mainFrame = basalt.createFrame()
 
-local modem = peripheral.find("modem") or error("No modem attached", 0)
+local modem = peripheral.find("modem") or error("No modem attached, please attach a modem (Preferably Ender modem).", 0)
 
-local aButton = main:addButton()
-        :setText("Host chat")
-        :setPosition(4, 4)
-        :setSize(16, 4)
+local HostButton = main:addButton()
+HostButton:setText("Host chat")
+HostButton:setPosition(4, 4)
+
+local chat_id = 1
+
+local chat_id_field = main:addInput()
+chat_id_field:setInputType("number")
+chat_id_field:setInputLimit(4)
+chat_id_field:setDefaultText("Channel ID")
+chat_id_field:setPosition(4, 8)
+
+HostButton:onClick(function(self,event,button,x,y)
+  if(event=="mouse_click")and(button==1)then
+    local ServerFrame = basalt.createFrame()
+  end
+end)
 
 basalt.autoUpdate()
-
-
-
-
-
