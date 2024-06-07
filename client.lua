ad6c3c2105a3173608769fd28f2b96d6ae3d1413
nohlstd <kuba.pawel44@gmail.com>
2024-06-07 20:08:51 +0200

    updated

--- client.lua
+++ client.lua
@@ -5,3 +5,7 @@ local basalt = require("basalt")
 local main = basalt.createFrame()
 local mainFrame = basalt.createFrame()
 local background = main:addPane():setSize(500, 500):setPosition():setBackground(colors.green, "#", colors.orange)
+
+-- Modem check
+
+local modem = peripheral.find("modem") or error("No modem attached, please attach a modem (Preferably Ender modem).", 0)
