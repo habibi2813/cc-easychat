f80768ec152c7e9428e6732b02ac5c69d8a3d634
nohlstd <kuba.pawel44@gmail.com>
2024-06-07 19:58:38 +0200

    update

--- server.lua
+++ server.lua
@@ -38,6 +38,33 @@ password_field:setPosition(4, 10)
 HostButton:onClick(function(self,event,button,x,y)
   if(event=="mouse_click")and(button==1)then
     modem.open(chat_id_field:getValue())
+    
+    local new_window = basalt.createFrame()
+    new_window:setTitle("Chat")
+    new_window:setSize(50, 20)
+    new_window:setPosition(8, 8)
+    
+    local chat_field = new_window:addInput()
+    chat_field:setInputType("text")
+    chat_field:setInputLimit(10)
+    chat_field:setDefaultText("Write message...")
+    chat_field:setPosition(4, 4)
+    
+    chat_field:onInputFinished(function(self,event,text)
+      if(text~="")then
+        modem.send(chat_id_field:getValue(), text)
+        chat_field:setText("")
+      end
+    end)
+    
+    modem.startEventHandling()
+    modem.on("message", function(event, id, message, distance)
+      if(id==chat_id_field:getValue())then
+        chat_field:setText("")
+        new_window:addLabel():setText(message):setPosition(4, 13)
+      end
+      
+    end)
   end
 end)
 
