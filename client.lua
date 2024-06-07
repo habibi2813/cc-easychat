01b6ea95fb962ebcfe89fe82610bf436e9cbff21
nohlstd <kuba.pawel44@gmail.com>
2024-06-07 20:16:49 +0200

    commit

--- client.lua
+++ client.lua
@@ -9,3 +9,19 @@ local background = main:addPane():setSize(500, 500):setPosition():setBackground(
 -- Modem check
 
 local modem = peripheral.find("modem") or error("No modem attached, please attach a modem (Preferably Ender modem).", 0)
+
+-- Chat ID Field
+
+local chat_id_field = main:addInput()
+chat_id_field:setInputType("number")
+chat_id_field:setInputLimit(4)
+chat_id_field:setDefaultText("Channel ID")
+chat_id_field:setPosition(4, 8)
+
+-- Password field
+
+local password_field = main:addInput()
+password_field:setInputType("password")
+password_field:setInputLimit(8)
+password_field:setDefaultText("Password")
+password_field:setPosition(4, 10)
