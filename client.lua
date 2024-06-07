356c0da14dbd0f647f45a39ee35a2f6f7face6dc
nohlstd <kuba.pawel44@gmail.com>
2024-06-07 20:08:06 +0200

    updated client.lua

---
+++ client.lua
@@ -0,0 +1,7 @@
+-- Import basalt
+
+local basalt = require("basalt")
+
+local main = basalt.createFrame()
+local mainFrame = basalt.createFrame()
+local background = main:addPane():setSize(500, 500):setPosition():setBackground(colors.green, "#", colors.orange)
